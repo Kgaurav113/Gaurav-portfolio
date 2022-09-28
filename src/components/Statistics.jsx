918206e483fa9309cf5ee44e0ca3c698fa0e4b90
Kumar Gaurav <kgaurav501@gmail.com>
2022-09-29 00:39:00 +0530

    fianl

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -7,19 +7,19 @@ const Statistics = () => {
     <div className="stat">
             <div className="individualStat">
                        <div>1200+</div>
-                        <div><h3>Hours</h3>of Coding</div>
+                        <div><br/><h3>Hours</h3>of Coding</div>
             </div>
            <div className="individualStat">
                       <div >600+</div>
-                     <div><h3>DSA</h3>Problems</div>
+                     <div><br/><h3>DSA</h3>Problems</div>
            </div>
             <div className="individualStat">
                     <div >10+</div>
-                       <div><h3>Projects</h3>Done</div>
+                       <div><br/><h3>Projects</h3>Done</div>
             </div>
             <div className="individualStat" id="hours">
                          <div>100+</div>
-                          <div><h3>Hours</h3> of Soft Skills Sessions</div>
+                          <div><br/><h3>Hours</h3> of Soft Skills Sessions</div>
             </div>
        </div>
     </div>
